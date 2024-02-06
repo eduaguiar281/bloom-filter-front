fbf82c8ae9bfff3fbedc326440b836b2fcb13799
eduaguiar281 <eduaguiar281@hotmail.com>
2024-02-06 13:41:07 -0300

    Correção Validação de exipiração

--- src/components/LocalStorageManager.js
+++ src/components/LocalStorageManager.js
@@ -11,26 +11,21 @@ export class LocalStorageManager extends Dexie {
   
     async setWithExpiration(key, value, expirationInSeconds) {
       const now = new Date().getTime();
-      const expirationTime = now + expirationInSeconds * 1000;
-  
-      await this.storage.add({ key, value, expirationTime })
+      const expiration = now + expirationInSeconds * 1000;
+      await this.storage.add({ key, value, expiration });
     }
   
     async getWithExpiration(key) {
       const storedData = await this.storage.where('key').equalsIgnoreCase(key).first()
       if (storedData) {
-//        const now = new Date().getTime();
-        return storedData.value;
-  
-//        if (now < storedData.expiration) {
-//          return storedData.value;
-//        } else {
-//          console.log('Excluindo....');
-//          await this.storage.delete(storedData.key);
-//          return storedData.value;
-//        }
+        const now = new Date().getTime();
+        if (now < storedData.expiration) {
+          return storedData.value;
+        } else {
+          await this.storage.delete(storedData.key);
+          return storedData.value;
+        }
       }
-  
       return null;
     }
     async hasKey(key) {
